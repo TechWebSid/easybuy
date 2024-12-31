2096108b60d4e65de314e87bd9d65a878c233081
TechWebSid <siddharthasrivastava30@gmail.com>
2024-12-31 21:59:43 +0530

    define routes

--- using-pages-router-with-javascript/jumpx/components/Services/ServicesStyleTwo.js
+++ using-pages-router-with-javascript/jumpx/components/Services/ServicesStyleTwo.js
@@ -7,42 +7,42 @@ const servicesData = [
     title: "Customize Website",
     shortText:
       "Start to Create visually stunning and responsive websites that reflect your brand identity and enhance user experience starting with low budget.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/customize-website/",
   },
   {
     iconName: "flaticon-vr",
     title: "E-commerce solutions",
     shortText:
       "Easy2BuyHub simplifies e-commerce by offering product listings, platform tie-ups, supplier networks, and comprehensive digital marketing.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/e-commerce/",
   },
   {
     iconName: "flaticon-blockchain",
     title: "SEO optimization",
     shortText:
       "Boost your website's visibility and attract organic traffic through expert SEO strategies, improving rankings and engagement.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/seo/",
   },
   {
     iconName: "flaticon-target",
     title: "Full Stack Development",
     shortText:
       "Deliver scalable and high-performance applications using modern technologies like React.js, Node.js, MongoDB, Next.js, Capacitor.js.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/fullstack/",
   },
   {
     iconName: "flaticon-choice",
     title: "API Integration",
     shortText:
       "Enhance your applications by integrating third-party APIs seamlessly, improving functionality, enriching user experience, automating processes.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/api/",
   },
   {
     iconName: "flaticon-deep-learning",
     title: "Digital Marketing",
     shortText:
       "Accelerating growth through customized strategies and powerful digital solutions designed to elevate your business and maximize results.",
-    viewDetails: "/services/service-details/",
+    viewDetails: "/services/digital-marketing/",
   },
 ];
 
