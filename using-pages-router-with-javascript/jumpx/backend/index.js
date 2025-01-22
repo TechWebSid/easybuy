88303c1fa3bbd521e715aa6fa4fcb6efec627ccf
TechWebSid <siddharthasrivastava30@gmail.com>
2025-01-22 11:24:54 +0530

    ..

--- using-pages-router-with-javascript/jumpx/backend/index.js
+++ using-pages-router-with-javascript/jumpx/backend/index.js
@@ -22,7 +22,7 @@ app.use(cors({
    // Specifically allow your frontend origin
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization']   
 }));
 
 app.use(express.json());
